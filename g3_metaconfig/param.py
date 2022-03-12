5a75b63986fa42e70cdc7863b1d7436e20e1c4f9
AJIADb9 <ajiadb9@bgd.team>
2022-03-12 20:52:03 +0300

    Moving Param setters inside it's class

--- g3_metaconfig/param.py
+++ g3_metaconfig/param.py
@@ -1,6 +1,10 @@
-from typing import Any, Callable, Optional, List, Union, Tuple
+import logging
+from typing import Any, Callable, Optional, List, Union, Tuple, Generator
 
+import configargparse
 from pydantic import BaseModel, Extra
+
+log = logging.getLogger("g3-param")
 
 
 class Param(BaseModel, extra=Extra.allow):
@@ -17,6 +21,54 @@ class Param(BaseModel, extra=Extra.allow):
     dest: str = None
     env_var: str = None
 
-    def __init__(self, *args, **kwargs, ):
+    def __init__(self, *args, **kwargs):
+        name = kwargs.pop("name", None)
+
         super().__init__(**kwargs, )
         self.args = list(args)
+
+        self._name = name
+
+    def _iter(self, *args, **kwargs) -> Generator[Tuple[str, Any], None, None]:
+        exclude: set = kwargs.get("exclude", None) or set()
+        exclude.add("_name")
+        return super()._iter(*args, **kwargs)
+
+    def set_name(self, param_name: str):
+        self._name = param_name
+
+    def set_dest(self):
+        if not self.dest:
+            self.dest = self._name
+
+    def set_args(self, auto_replace_underscores_with_dashes: bool):
+        if not self.args:
+            cli_name = self._name.lower()
+            if auto_replace_underscores_with_dashes:
+                cli_name = cli_name.replace('_', '-')
+            self.args = [f"--{cli_name}"]
+
+    def set_env(self, env_prefix: Optional[str] = None):
+        if all([env_prefix is not None, self.env_var is None, ]):
+            self.env_var = f"{env_prefix}{self._name}".upper()
+
+    def set_type(self, auto_typing: bool, annotation: Optional[Any], parser: Union[configargparse.ArgParser, Any]):
+        # Checking auto_typing compatibility with selected action
+        if auto_typing and self.action is not None:
+            try:
+                action_class = parser._registry_get("action", self.action, self.action)
+                action_class(
+                    option_strings=self.args,
+                    type="test",
+                    **self.dict(exclude={"args", "action"}, exclude_unset=True),
+                )
+            except TypeError as e:
+                auto_typing = False
+                log.warning(
+                    f"Param '{self._name}' has action={self.action} "
+                    f"which leads to disabling Config.auto_typing for this variable"
+                )
+
+        if all([auto_typing, self.type is None, annotation is not None]):
+            if isinstance(annotation, type):
+                self.type = annotation
--- g3_metaconfig/param.pyi
+++ g3_metaconfig/param.pyi
@@ -1,5 +1,7 @@
-from typing import Union, Any, Optional, Callable, Tuple, List
-from pydantic import BaseModel, Extra
+from typing import Union, Any, Optional, Callable, Tuple, List, Generator
+
+import configargparse
+from pydantic import BaseModel, Extra, Field
 
 
 class Param(BaseModel, extra=Extra.allow):
@@ -16,9 +18,12 @@ class Param(BaseModel, extra=Extra.allow):
     dest: str = None
     env_var: str = None
 
+    _name = Field()
+
     def __init__(
             self,
             *args,
+            name: str = None,
             action: str = None,
             nargs: Union[int, str] = None,
             const: str = None,
@@ -31,4 +36,22 @@ class Param(BaseModel, extra=Extra.allow):
             dest: str = None,
             env_var: str = None,
             **kwargs, ):
+        ...
+
+    def _iter(self, *args, **kwargs) -> Generator[Tuple[str, Any], None, None]:
+        ...
+
+    def set_name(self, param_name: str):
+        ...
+
+    def set_dest(self):
+        ...
+
+    def set_args(self, auto_replace_underscores_with_dashes: bool):
+        ...
+
+    def set_env(self, env_prefix: Optional[str] = None):
+        ...
+
+    def set_type(self, auto_typing: bool, annotation: Any, parser: Union[configargparse.ArgParser, Any]):
         ...
